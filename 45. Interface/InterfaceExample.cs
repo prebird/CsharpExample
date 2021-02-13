0c5f9956d9852eac3b530324a75ef1d800ed5f48
yonggyujeong <yonggyujeong10@gmail.com>
2021-02-13 17:21:15 +0900

    45강 인터페이스, 46강 어트리뷰트&리플랙션 추가

---
+++ 45. Interface/InterfaceExample.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+namespace Interface
+{
+    //관례적으로 모든 인터페이스는 "I"로 시작
+    public interface ICarStandard 
+    {
+        void Stop(); //액세스 한정자 붙히지 않음
+    }
+    abstract class KS
+    {
+        public abstract void Back(); //본문 선언불가 ,설계만
+        //public void Fly() => WriteLine("날다"); // 구현도 가능
+    }
+
+    sealed class Car : KS, ICarStandard
+    {
+        public void Go() => WriteLine("가다");
+        public override void Back() => WriteLine("후진");
+        public void Stop() => WriteLine("멈춤");
+    }
+
+    class InterfaceExample
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.Go(); car.Back(); ; car.Stop();
+        }
+    }
+}
---
+++ 46. AttributeAndReflection/AttributeAndReflection.cs
@@ -0,0 +1,46 @@
+﻿
+#define RELEASE // Release 모드 임을 가정
+
+using System;
+using System.Diagnostics;
+using static System.Console;
+using System.Reflection;
+
+namespace AttributeAndReflection
+{
+    // 사용자 정의어트리뷰트 생성 - 멤버가 없으면 표시만하는 기능
+    public class LuxuryAttribute : Attribute 
+    {
+        public LuxuryAttribute() => WriteLine("럭셔리!!");
+    }
+
+    [Luxury]
+    class Car
+    {
+        [Obsolete("다음 버전에 제거될 예정이니 Auto()를 사용하세요")] 
+        public void Manual() => WriteLine("수동운전");    // 1.0 verison
+        // 주석처리와 달리 밑의 코드들이 에러가 나지 않음 
+        public void Auto() => WriteLine("자동운전"); // 2.0 version
+        [Conditional("DEBUG")] public void Test() => WriteLine("테스트 운전");
+        [Conditional("RELEASE")] public void Test2() => WriteLine("테스트 운전");
+
+    }
+    class AttributeAndReflection
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            Attribute.GetCustomAttributes(typeof(Car));
+            typeof(Car).GetCustomAttributes(false);
+            car.Manual(); car.Auto();
+            car.Test(); car.Test();
+
+            //리플랙션 : 모든 멤버를 다 불러올 수 있음
+            // 라이브러리나 API를 만들때 사용됨
+            var carType = new Car();
+            Type mycar = carType.GetType();
+            MethodInfo info = mycar.GetMethod("Auto");
+            info.Invoke(carType, null);
+        }
+    }
+}
