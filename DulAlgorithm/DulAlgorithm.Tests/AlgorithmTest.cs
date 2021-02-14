56fb55b724b03369e958f535c1d351c4b01fa113
yonggyujeong <yonggyujeong10@gmail.com>
2021-02-14 20:26:16 +0900

    MStest 이용한 초간단 API 테스트

---
+++ DulAlgorithm/DulAlgorithm.Tests/AlgorithmTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DulAlgorithm.Tests
+{
+    [TestClass]
+    public class AlgorithmClassTest
+    {
+        
+        [TestMethod]
+        public void SelectionSortMethodTest()
+        {
+            //[1] Arrange(정렬), Setup
+            int[] arr = { 33, 22, 44 };
+
+            //[2] Act(동작), Execute
+            int[] results = DulAlgorithm.Algorithm.SelectionSort(arr);
+
+            //[3] Assert(어설션), Verify
+            Assert.AreEqual(22, results[0]); //기대값, 실제값
+            Assert.AreEqual(44, results[results.Length - 1]);
+
+        }
+    }
+}
