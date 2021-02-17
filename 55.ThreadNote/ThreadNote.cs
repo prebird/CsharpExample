b84dff4edacbb7142ca557712468fcaa5052b3cb
yonggyujeong <yonggyujeong10@gmail.com>
2021-02-17 23:18:56 +0900

    55장 스레드 추가, 하는중

---
+++ 55.ThreadNote/ThreadNote.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CsharpExample
+{
+    class Restaurant
+    {
+        public void MakeFood()
+        {
+            Console.WriteLine($"요리 시작");
+            Console.WriteLine($"요리 종료");
+        }
+    }
+
+    class ThreadNote
+    {
+        static void Main()
+        {
+            (new Restaurant()).MakeFood();
+        }
+    }
+}
